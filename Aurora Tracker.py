d6097405b5d65607019960baa4c91cc061a25ede
JakobSchoeberle <jrschoeberle@gmail.com>
2024-05-14 17:18:45 -0500

    Cleaned up a bit

--- Aurora Tracker.py
+++ Aurora Tracker.py
@@ -21,28 +21,21 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 async def on_ready():
     await bot.tree.sync()
 
-@bot.tree.command(name="hello", description="Hello World?")
-async def slash_command(interaction:discord.Interaction):
-    await interaction.response.send_message("Hello World!")
-
 @bot.tree.command(name="tonight", description="Forecasted Aurora Viewline for Tonight")
 async def slash_command(interaction:discord.Interaction):
     data_url = "https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png"
-    urllib.request.urlretrieve(data_url, "tonights_static_viewline_forecast.png")
-    await interaction.response.send_message(file=discord.File('tonights_static_viewline_forecast.png'))
+    pathed_filename = os.path.join("./_media", "tonights_static_viewline_forecast.png")
+    urllib.request.urlretrieve(data_url, pathed_filename)
+    await interaction.response.send_message(file=discord.File(pathed_filename))
 
 @bot.tree.command(name="tomorrow", description="Forecasted Aurora Viewline for Tomorrow")
 async def slash_command(interaction:discord.Interaction):
     data_url = "https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tomorrow_nights_static_viewline_forecast.png"
-    urllib.request.urlretrieve(data_url, "tomorrow_nights_static_viewline_forecast.png")
-    await interaction.response.send_message(file=discord.File('tomorrow_nights_static_viewline_forecast.png'))
+    pathed_filename = os.path.join("./_media", "tomorrow_nights_static_viewline_forecast.png")
+    urllib.request.urlretrieve(data_url, pathed_filename)
+    await interaction.response.send_message(file=discord.File(pathed_filename))
 
 @bot.tree.command(name="help", description="Help with using the Aurora Tracker")
-async def slash_command(interaction:discord.Interaction):
-    embeded=discord.Embed(title="Aurora Borealis Tracker Commands", description="This bot is designed to provide Aurora Borealis Forecast in Discord.\n\n**Commands:**\n•`/tonight` This displays the NOAA Forcast for tonight\n\n•`/tomorrow` This displays the NOAA Forcast for tomorrow\n\n•`/help` You just used this command", color=0xe100ff)
-    await interaction.response.send_message(embed=embeded)
-
-@bot.tree.command(name="Dev", description="Testing Command")
 async def slash_command(interaction:discord.Interaction):
     embeded=discord.Embed(title="Aurora Borealis Tracker Commands", description="This bot is designed to provide Aurora Borealis Forecast in Discord.\n\n**Commands:**\n•`/tonight` This displays the NOAA Forcast for tonight\n\n•`/tomorrow` This displays the NOAA Forcast for tomorrow\n\n•`/help` You just used this command", color=0xe100ff)
     await interaction.response.send_message(embed=embeded)
