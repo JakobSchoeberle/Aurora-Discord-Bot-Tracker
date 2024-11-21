776aae62719c3357ab572c4a2754c8e77a5f80fd
JakobSchoeberle <jrschoeberle@gmail.com>
2024-11-20 21:27:03 -0600

    Old Changes

--- AuroraToolbox.py
+++ AuroraToolbox.py
@@ -5,28 +5,55 @@ import urllib.request
 from urllib.request import urlopen 
 from PIL import Image
 
+def ImageRetriever(imagepath: str, mediapath: str):
+    """Goes to (https://services.swpc.noaa.gov) to retrieve images using the url path to that image and the path to where to download it to"""
+    data_url = "https://services.swpc.noaa.gov/" + imagepath
+    pathed_filename = os.path.join(mediapath, imagepath.rsplit('/', 1)[1])
+    urllib.request.urlretrieve(data_url, pathed_filename)
+    return pathed_filename
+
+'''def ImageRetriever(imagepath: str, imagename: str, mediapath: str):
+    """Goes to (https://services.swpc.noaa.gov) to retrieve images using the url path to that image and the path to where to download it to"""
+    data_url = "https://services.swpc.noaa.gov/" + imagepath
+    pathed_filename = os.path.join(mediapath, imagename)
+    urllib.request.urlretrieve(data_url, pathed_filename)
+    return pathed_filename'''
+
 def make_gif(frame_folder):
     frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.jpg")]
     frame_one = frames[0]
     pathed_filename = os.path.join("./_media", "Aurora_North.gif")
     frame_one.save(pathed_filename, format="gif", append_images=frames, save_all=True, duration=100, loop=0)
     
-def jsonParser():
-    url = "https://services.swpc.noaa.gov/products/animations/ovation_north_24h.json"
-    json_url = urlopen(url)
-    data = json.loads(json_url.read())
-    lenght=len(data)
-    for index in range(500, lenght):
-        x = json.dumps(data[index])
+def jsonParser(myPath):
+    jsonurl = "https://services.swpc.noaa.gov/products/animations/ovation_north_24h.json"
+    jsons = urlopen(jsonurl)
+    jsondata = json.loads(jsons.read())
+    lenght=len(jsondata)
+    for index in range(0, lenght):
+        x = json.dumps(jsondata[index])
         #await ctx.send(data[0])
         y = json.loads(x)
         data_url = "https://services.swpc.noaa.gov/" + y["url"]
         image_name = "aurora_North_" + str(index) + ".jpg"
-        myPath = "./image"
         pathed_filename = os.path.join(myPath, image_name)
         urllib.request.urlretrieve(data_url, pathed_filename)
     return(myPath)
-
+'''
+def jsonParser(jsonurl: str, mediapath: str):
+    json = urlopen(jsonurl)
+    jsondata = json.loads(json.read())
+    jsonlength = len(jsondata)
+    for index in range(500, jsonlength):
+        x = json.dumps(jsondata[index])
+        #await ctx.send(data[0])
+        y = json.loads(x)
+        data_url = "https://services.swpc.noaa.gov/" + y["url"]
+        image_name = "aurora_North_" + str(index) + ".jpg"
+        pathed_filename = os.path.join(mediapath, image_name)
+        urllib.request.urlretrieve(data_url, pathed_filename)
+    return()
+'''
 def VideoProducer(t_0, t_w, f_w): # NOT DONE
     """Example."""
     GIF_Name = "Aurora_North.gif"
--- Bot.py
+++ Bot.py
@@ -21,9 +21,7 @@ async def on_ready():
 
 @bot.tree.command(name="tonight", description="Forecasted Aurora Viewline for Tonight")
 async def slash_command(interaction:discord.Interaction):
-    data_url = "https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png"
-    pathed_filename = os.path.join("./_media", "tonights_static_viewline_forecast.png")
-    urllib.request.urlretrieve(data_url, pathed_filename)
+    pathed_filename = Toolbox.ImageRetriever("experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png", "./_media")
     await interaction.response.send_message(file=discord.File(pathed_filename))
 
 @bot.tree.command(name="tomorrow", description="Forecasted Aurora Viewline for Tomorrow")
@@ -33,23 +31,35 @@ async def slash_command(interaction:discord.Interaction):
     urllib.request.urlretrieve(data_url, pathed_filename)
     await interaction.response.send_message(file=discord.File(pathed_filename))
 
-@bot.tree.command(name="now", description="The current Aurora prediction for your moment in time")
+@bot.tree.command(name="latest", description="The current Aurora prediction")
 async def slash_command(interaction:discord.Interaction):
+    pathed_filename = Toolbox.ImageRetriever("images/animations/ovation/north/latest.jpg", "./_media")
     embeded=discord.Embed(title="The Aurora Now", description="This is not done yet", color=0xe100ff)
-    await interaction.response.send_message(embed=embeded)
+    await interaction.response.send_message(file=discord.File(pathed_filename), embed=embeded)
+
+#"https://services.swpc.noaa.gov/images/geospace/geospace_3_hour.png"
+
+#"https://services.swpc.noaa.gov/json/geospace/geospace_pred_est_kp_1_hour.json"
+
+#"https://services.swpc.noaa.gov/json/planetary_k_index_1m.json"
+
+#"https://services.swpc.noaa.gov/json/ovation_aurora_latest.json" 
 
 @bot.tree.command(name="help", description="Help with using the Aurora Tracker")
 async def slash_command(interaction:discord.Interaction):
     embeded=discord.Embed(title="Aurora Borealis Tracker Commands", description="This bot is designed to provide Aurora Borealis Forecast in Discord.\n\n**Commands:**\n•`/tonight` This displays the NOAA Forcast for tonight\n\n•`/tomorrow` This displays the NOAA Forcast for tomorrow\n\n•`/help` You just used this command", color=0xe100ff)
     await interaction.response.send_message(embed=embeded)
-'''
+
 @bot.command()
 async def ovation(ctx):
-    myPath = Toolbox.jsonParser()
-    Toolbox.make_gif(myPath)
-    pathed_filename = os.path.join("./_media", "Aurora_North.gif")
-    await ctx.send(file=discord.File(pathed_filename))
-'''
+    #myPath = Toolbox.jsonParser("./image")
+    #Toolbox.make_gif(myPath)
+    #pathed_filename = os.path.join("./_media", "Aurora_North.gif")
+    #await ctx.reply(file=discord.File(pathed_filename))
+    embeded=discord.Embed(title="Aurora Borealis Tracker Commands", description="This bot is designed to provide Aurora Borealis Forecast in Discord.\n\n**Commands:**\n•`/tonight` This displays the NOAA Forcast for tonight\n\n•`/tomorrow` This displays the NOAA Forcast for tomorrow\n\n•`/help` You just used this command", color=0xe100ff)
+    pathed_filename = Toolbox.ImageRetriever("experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png", "./_media")
+    await ctx.reply(file=discord.File(pathed_filename), embed=embeded)
+
 @bot.event # Removed so that it doesn't fuck up the ovation command since that takes so long
 async def on_command_error(ctx, error): # Not sure this dones anything anymore
     await ctx.send(str(error))
